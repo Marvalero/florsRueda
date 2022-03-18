3fab46782989bdeeb033ee90db6dab6e16c67bed
Marvalero <mariavalerocam@gmail.com>
2022-03-18 12:57:47 +0100

    First commit

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Flors Rueda</h1>
+	<h2>Aprende a motivar a tus alumnos usando las nuesvas tecnologias</h2>
 </main>
 
 <style>
